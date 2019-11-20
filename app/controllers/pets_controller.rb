7322b565f6c65cb6f50fc5e7aa7a609ea34eeccf
marcel fonseca <marcelsf23br@gmail.com>
2019-11-20 08:57:43 -0300

    add simple_form_for and bootstrap

---
+++ app/controllers/pets_controller.rb
@@ -0,0 +1,52 @@
+class PetsController < ApplicationController
+  # We find the pet in our Database with Active Record find method
+  before_action :set_pet, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @pets = Pet.all
+  end
+
+  def show
+  end
+
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new(pet_params)
+    @pet.save
+    redirect_to pet_path(@pet)
+  end
+
+  def edit
+  end
+
+  def update
+    @pet.update(pet_params)
+    redirect_to pets_path
+  end
+
+  def destroy
+    @pet.destroy
+    redirect_to pets_path
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :species, :address, :found_on)
+  end
+
+  def set_pet
+    @pet = Pet.find(params[:id])
+  end
+end
+
+
+
+
+
+
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+  puts 'Creating animals'
+
+  Pet.create(name: 'Rex', address: 'Gloria', species: 'dog', found_on: Time.now)
+  Pet.create(name: 'Tom', address: 'USA', species: 'cat', found_on: Time.now)
+
+  puts 'Seed complete'
